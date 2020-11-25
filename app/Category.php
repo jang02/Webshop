d74b72d87728595249adce60402b825cbd8ea32d
jan <jangarretsen01@gmail.com>
2020-11-25 14:26:22 +0100

    added getproducts function

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function getProducts(): array {
+        $result = [];
+        foreach(Product::all() as $product) {
+            if ($product->category_id === $this->id) {
+                array_push($result, $product);
+            }
+        }
+        return $result;
+    }
 }
